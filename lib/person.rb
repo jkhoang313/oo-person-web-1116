f126e89e1587f0a00350b94f305d26c9a616bb6f
jkhoang313 <jkhoang313@gmail.com>
2016-11-30 22:09:34 -0500

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,76 @@
 # your code goes here
+class Person
+  attr_reader :name
+  attr_writer :happiness, :hygiene
+  attr_accessor :bank_account
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def set_points(attribute)
+    if attribute > 10
+      10
+    elsif attribute < 0
+      0
+    else
+      attribute
+    end
+  end
+
+  def happiness
+    set_points(@happiness)
+  end
+
+  def hygiene
+    set_points(@hygiene)
+  end
+
+  def happy?
+    happiness > 7 ? true : false
+  end
+
+  def clean?
+    hygiene > 7 ? true : false
+  end
+
+  def get_paid(salary)
+    self.bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+    case topic
+    when "politics"
+      self.happiness -= 2
+      friend.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    when "weather"
+      self.happiness += 1
+      friend.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+end
